fe96579cc46450ff63cd4034b791de92e09181ea
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-15 13:14:15 -0300

    Add Firebase Config Object

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: process.env.API_KEY,
+  authDomain: 'order-x.firebaseapp.com',
+  databaseURL: 'https://order-x.firebaseio.com',
+  projectId: 'order-x',
+  storageBucket: 'order-x.appspot.com',
+  messagingSenderId: '821831373562',
+  appId: '1:821831373562:web:6a106686d2554e43f8ef43'
+}
