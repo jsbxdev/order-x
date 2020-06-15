607af66aa19ce1e38424dc7dbd33a9b7111f870a
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-15 13:13:58 -0300

    Add Firestore module

---
+++ firestore.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import initializeApp from '~/firebase'
+initializeApp()
+
+// Enabable persistence
+firebase
+  .firestore()
+  .enablePersistence()
+  .catch(function(err) {
+    if (err.code === 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a a time.
+      // ...
+    } else if (err.code === 'unimplemented') {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+    }
+  })
+
+// Firestore
+export const database = firebase.firestore()
+export const organizationsRef = database.collection('organizations')
+export const usersRef = database.collection('users')
