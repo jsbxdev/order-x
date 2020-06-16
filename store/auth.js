8845023548e6c3ec61326be3993c678c1690526e
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-16 13:20:57 -0300

    Create auth store and localStorage persist methods

---
+++ store/auth.js
@@ -0,0 +1,77 @@
+import localStorage from 'store2'
+import { firestoreAction } from 'vuexfire'
+
+export const state = () => ({
+  user: false,
+  authVerified: false
+})
+
+export const getters = {
+  isAuthVerified: (state) => !!state.authVerified,
+  isUserAdmin: (state) => (state.user ? !!state.user.isAdmin : false),
+  isUserSigned: (state) => state.authVerified,
+  getUser: (state) => state.user,
+  getUserId: (state) => (state.user ? state.user.uid : false),
+  getUserPhotoURL: (state) => (state.user ? state.user.photoURL : false),
+  getUserOrganizationId: (state) => state.user?.organizationId
+}
+
+export const mutations = {
+  SET_AUTH_VERIFIED: (state, value) => (state.authVerified = !!value),
+  SET_USER: (state, user = false) => (state.user = user)
+}
+
+export const actions = {
+  bindUser: firestoreAction(async (context, userId = false) => {
+    const localStorageUser = !localStorage('user')
+    userId = userId || localStorageUser.uid
+    if (userId) {
+      const { usersRef } = await import('~/firestore')
+      context.bindFirestoreRef('user', usersRef.doc(userId))
+    }
+  }),
+  bindUserFromLocalStorage: ({ commit, dispatch }) => {
+    commit('SET_USER', localStorage('user') || false)
+  },
+  verifyFirebaseAuth: async ({ dispatch, commit }) => {
+    const { auth } = await import('~/auth')
+    const { usersRef } = await import('~/firestore')
+    auth.onAuthStateChanged(function(authUser) {
+      commit('SET_AUTH_VERIFIED', true)
+      if (authUser) {
+        const user = {
+          ...authUser.providerData[0],
+          uid: authUser.uid
+        }
+        usersRef
+          .doc(user.uid)
+          .set(user, { merge: true })
+          .then(() => {
+            dispatch('bindUser', user.uid)
+          })
+      } else {
+        commit('SET_USER', false)
+        dispatch('setLocalStorage', false)
+      }
+    })
+  },
+  setLocalStorage: (context, user = false) => {
+    if (user?.uid) {
+      localStorage('user', user)
+    } else {
+      localStorage('user', false)
+    }
+  },
+  signIn: async (context, providerId) => {
+    const { auth } = await import('~/auth')
+    try {
+      if (providerId === 'google') {
+        const { googleAuthProvider } = await import('~/auth')
+        auth.signInWithRedirect(googleAuthProvider)
+      } else if (providerId === 'facebook') {
+        const { facebookAuthProvider } = await import('~/auth')
+        auth.signInWithRedirect(facebookAuthProvider)
+      }
+    } catch {}
+  }
+}
