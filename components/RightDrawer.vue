b45ad12a2d2599b89f3c569861e27a944112fe43
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-16 13:21:50 -0300

    Create RightDrawer component

---
+++ components/RightDrawer.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-navigation-drawer
+    v-model="rightDrawer"
+    color="red"
+    app
+    clipped
+    right
+    floating
+  >
+    <v-text-field name="name" label="label" id="id"></v-text-field>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  computed: {
+    rightDrawer: {
+      get() {
+        return this.$store.state.ui.rightDrawer
+      },
+      set(value) {
+        this.$store.commit('ui/SET_RIGHT_DRAWER', value)
+      }
+    }
+  }
+}
+</script>
+
+<style></style>
