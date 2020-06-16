44072edbaac419d6b73dd75175da60ac9b098b71
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-16 13:21:38 -0300

    Create NavigationDrawer component

---
+++ components/NavigationDrawer.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    color="#121212"
+    app
+    clipped
+    :expand-on-hover="!isMobile && miniVariant"
+    :mini-variant="!isMobile && miniVariant"
+    floating
+  >
+    <v-list shaped>
+      <v-list-item link to="/">
+        <v-list-item-icon>
+          <v-icon>mdi-view-comfy</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>Pedidos</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link to="/menu">
+        <v-list-item-icon>
+          <v-icon>mdi-food</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>Cardápio</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  computed: {
+    drawer: {
+      get() {
+        return this.$store.state.ui.drawer
+      },
+      set(value) {
+        this.$store.commit('ui/SET_DRAWER', this.isMobile ? value : true)
+      }
+    },
+    miniVariant() {
+      return this.$store.getters['ui/drawerMiniVariant']
+    },
+    isMobile() {
+      return this.$store.getters['ui/isMobile']
+    }
+  }
+}
+</script>
+
+<style></style>
