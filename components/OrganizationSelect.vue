d3c66de78bf3eee6cbf588eaade8ddcad9144ea8
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-16 13:22:03 -0300

    Create OrganizationSelect component

---
+++ components/OrganizationSelect.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card v-if="!hasOrganization" outlined>
+    <v-container>
+      <p class="text-center">
+        Você não está conectado com nenhuma organização, peça para te
+        adicionarem pelo e-mail:
+      </p>
+      <p class="text-center">{{ userEmail }}</p>
+      <p>
+        {{ hasOrganization }}
+      </p>
+      <v-btn color="success" @click="createOrganization">
+        Criar Organização
+      </v-btn>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  computed: {
+    user() {
+      return this.$store.getters['auth/getUser']
+    },
+    userEmail() {
+      return this.user?.email
+    },
+    hasOrganization() {
+      return this.user?.organizationId
+    }
+  },
+  methods: {
+    createOrganization() {
+      this.$router.push({ name: 'organization-create' })
+    }
+  }
+}
+</script>
+
+<style></style>
