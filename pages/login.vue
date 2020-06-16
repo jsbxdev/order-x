cf32e614de32d71ed4e8d801487c8af1519e3937
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-16 13:20:21 -0300

    Create login page

---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout>
+    <v-btn color="success" @click="signInWithGoogle">Entrar com Google</v-btn>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  methods: {
+    signInWithGoogle() {
+      this.$store.dispatch('auth/signIn', 'google')
+    }
+  }
+}
+</script>
+
+<style></style>
