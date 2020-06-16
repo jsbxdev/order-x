2c23403424c3e8a65b39d3c000b6ec587e5d7c34
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-16 13:21:26 -0300

    Create AppBar component

---
+++ components/AppBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app-bar
+    class="app-bar"
+    color="transparent"
+    elevation="0"
+    app
+    clipped-left
+    clipped-right
+    tile
+  >
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+    <v-btn icon @click="rightDrawer = !rightDrawer">
+      <v-icon v-if="!rightDrawer">mdi-chevron-left</v-icon>
+      <v-icon v-else>mdi-chevron-right</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  computed: {
+    drawer: {
+      get() {
+        return this.$store.state.ui.drawer
+      },
+      set(value) {
+        this.$store.commit('ui/SET_DRAWER', value)
+      }
+    },
+    rightDrawer: {
+      get() {
+        return this.$store.state.ui.rightDrawer
+      },
+      set(value) {
+        this.$store.commit('ui/SET_RIGHT_DRAWER', value)
+      }
+    },
+    isMobile() {
+      return this.$store.getters['ui/isMobile']
+    }
+  }
+}
+</script>
+
+<style>
+@media only screen and (min-width: 600px) {
+  .app-bar.v-app-bar {
+    border-bottom: solid 1px #757575 !important;
+  }
+}
+</style>
