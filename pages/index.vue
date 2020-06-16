4b0b4186e5dff0b5ffc3182407d2d2ef3f18b686
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-16 13:19:56 -0300

    Create home page

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-layout column justify-center>
+    <organization-select></organization-select>
+  </v-layout>
+</template>
+
+<script>
+import OrganizationSelect from '~/components/OrganizationSelect.vue'
+export default {
+  components: {
+    OrganizationSelect
+  }
+}
+</script>
