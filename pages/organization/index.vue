9c01af03968e30ebe7830cb80107bb83a0338837
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-17 14:29:44 -0300

    Create organization page

---
+++ pages/organization/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-row dense>
+    <v-col>
+      <h1>Organização</h1>
+    </v-col>
+    <v-col cols="12">
+      <v-subheader>Informações</v-subheader>
+      <v-card outlined>
+        <organization-form :organization="organization"></organization-form>
+      </v-card>
+    </v-col>
+    <v-col cols="12">
+      <v-subheader>Usuários</v-subheader>
+      <v-card outlined>
+        <v-container>
+          <search-user></search-user>
+          <v-subheader>Usuários Ativos</v-subheader>
+        </v-container>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import OrganizationForm from '~/components/OrganizationForm.vue'
+import SearchUser from '~/components/SearchUser.vue'
+export default {
+  components: {
+    OrganizationForm,
+    SearchUser
+  },
+  data() {
+    return {
+      valid: false,
+      data: {}
+    }
+  },
+  computed: {
+    organization() {
+      return this.$store.getters['organization/getOrganization']
+    }
+  },
+  watch: {
+    organization: {
+      immediate: true,
+      handler(organization) {
+        if (organization) {
+          this.data = { ...organization }
+        }
+      }
+    }
+  },
+  methods: {
+    update() {
+      if (this.$refs.form.validate()) {
+      }
+    }
+  }
+}
+</script>
+
+<style></style>
