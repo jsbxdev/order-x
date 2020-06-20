7c0af7a9d2c4d95b02e637477f8717087aa89d3c
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-20 19:32:33 -0300

    Create ItemList component

---
+++ components/ItemList.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <item></item>
+  </v-container>
+</template>
+
+<script>
+import Item from '~/components/Item.vue'
+export default {
+  components: {
+    Item
+  }
+}
+</script>
+
+<style></style>
