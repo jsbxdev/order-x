eb55715b2d73cd8f7957cee25580a617f00d5224
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-20 19:34:23 -0300

    Add organization list

--- components/OrganizationSelect.vue
+++ components/OrganizationSelect.vue
@@ -1,23 +1,50 @@
 <template>
   <v-card v-if="!hasOrganization" outlined>
     <v-container>
-      <p class="text-center">
-        Você não está conectado com nenhuma organização, peça para te
-        adicionarem pelo e-mail:
-      </p>
-      <p class="text-center">{{ userEmail }}</p>
-      <p>
-        {{ hasOrganization }}
-      </p>
-      <v-btn color="success" @click="createOrganization">
-        Criar Organização
-      </v-btn>
+      <v-row>
+        <v-col cols="12">
+          <p class="text-center">
+            Você não está conectado com nenhuma organização, peça para te
+            adicionarem pelo e-mail:
+          </p>
+          <p class="text-center">{{ userEmail }}</p>
+        </v-col>
+        <v-col cols="12">
+          <v-list class="d-flex flex-column align-center">
+            <v-subheader>Selecione a organização:</v-subheader>
+            <v-progress-circular
+              v-if="!invites.length"
+              indeterminate
+              color="primary"
+            ></v-progress-circular>
+            <v-list-item
+              v-for="organization in invites"
+              :key="organization.id"
+              link
+              @click="acceptInvite(organization.id)"
+            >
+              <v-list-item-title>{{ organization.name }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-col>
+        <v-col cols="12" class="d-flex flex-column align-center">
+          <v-subheader>Ou crie uma nova organização:</v-subheader>
+          <v-btn color="success" block @click="createOrganization">
+            Criar Organização
+          </v-btn>
+        </v-col>
+      </v-row>
     </v-container>
   </v-card>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      invites: []
+    }
+  },
   computed: {
     user() {
       return this.$store.getters['auth/getUser']
@@ -29,7 +56,24 @@ export default {
       return this.user?.organizationId
     }
   },
+  watch: {
+    user: {
+      immediate: true,
+      async handler(user) {
+        if (user) {
+          const { organizationsRef } = await import('~/firestore')
+          this.$bind(
+            'invites',
+            organizationsRef.where('invitedUserIds', 'array-contains', user.uid)
+          )
+        }
+      }
+    }
+  },
   methods: {
+    acceptInvite(organizationId) {
+      this.$store.dispatch('auth/acceptInvite', organizationId)
+    },
     createOrganization() {
       this.$router.push({ name: 'organization-create' })
     }
