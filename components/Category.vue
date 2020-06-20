6ad802f6e0ba0395be49a9416ffcd7fbe2b493da
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-20 19:31:49 -0300

    Create Category component

---
+++ components/Category.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <category-form></category-form>
+  </v-container>
+</template>
+
+<script>
+import CategoryForm from '~/components/CategoryForm.vue'
+export default {
+  components: {
+    CategoryForm
+  }
+}
+</script>
+
+<style></style>
